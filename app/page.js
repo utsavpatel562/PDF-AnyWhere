9714cee8b3bae42969a77f79b1630b3b36388502
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-01-17 12:17:09 -0500

    NEW COMMIT

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <>
-    <div>
+    <div className="flex gap-5 p-10">
       <h1>Home</h1>
+      <Button>This testing button</Button>
     </div>
     </>
   );
