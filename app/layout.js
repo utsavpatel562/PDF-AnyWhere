d190c20331d027358f59b8ddad1b79c2a008272a
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-01-23 14:02:03 -0500

    NEW COMMIT

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import {Outfit} from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const outfit = Outfit({subsets: ['latin']})
 
 export const metadata = {
   title: "Create Next App",
@@ -20,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={outfit.className}
       >
         {children}
       </body>
