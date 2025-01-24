f8d3dee969795ce43950a21cf15d83542d07bf2d
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-01-24 15:00:51 -0500

    NEW COMMIT

--- app/provider.js
+++ app/provider.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 
 function Provider({children}) {
     const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
   return (
     <div>
         <ConvexProvider client={convex}>{children}</ConvexProvider>;
-        {children}
     </div>
   )
 }
