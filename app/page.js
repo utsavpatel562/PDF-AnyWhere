167a0907b66b8adb633c01e93af80d36e2afb133
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-01-27 09:50:41 -0500

    NEW COMMIT

--- app/page.js
+++ app/page.js
@@ -3,9 +3,14 @@ import { UserButton, useUser } from "@clerk/nextjs";
 import { Button } from "../components/ui/button";
 import {useMutation} from "convex/react";
 import { api } from "../convex/_generated/api";
+import { useEffect } from "react";
 export default function Home() {
   const {user} = useUser();
   const createUser = useMutation(api.user.createUser);
+
+  useEffect(()=> {
+    user&&CheckUser();
+  }, [user])
   
   const CheckUser = async() => {
     const result = await createUser({
