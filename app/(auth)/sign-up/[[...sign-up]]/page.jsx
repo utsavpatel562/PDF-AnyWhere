cc44e184688d975c867a5a79ada07363367d5cdb
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-01-27 09:01:10 -0500

    NEW COMMIT

--- app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -1,5 +1,11 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen bg-zinc-800">
+        <SignUp />
+      </div>
+    </>
+  );
 }
