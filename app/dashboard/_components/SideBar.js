b8896889adb26ef5fcb9364f35d24c2163d6a4bc
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-01-28 12:03:04 -0500

    NEW COMMIT

--- app/dashboard/_components/SideBar.js
+++ app/dashboard/_components/SideBar.js
@@ -1,11 +1,15 @@
 import Image from 'next/image'
 import React from 'react'
+import { Button } from '../../../components/ui/button'
 
 function SideBar() {
   return (
     <>
-    <div className='shadow-sm h-screen p-7'>
-        <Image src={"/logo2.png"} alt='pdfanywhere' width={200} height={200}/>
+    <div className='shadow-md border-2 border-r-gray-200 h-screen p-2'>
+        <Image src={"/logo2.png"} alt='pdfanywhere' width={200} height={200} className='p-1 mt-3 m-auto'/>
+        <div className='mt-10'>
+            <Button className="hover:bg-zinc-800 p-5 w-full">+ Upload PDF</Button>
+        </div>
     </div>
     </>
   )
