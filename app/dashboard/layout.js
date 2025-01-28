24818ab7a686d0a787d9aac1bb137ae5f27c2ec1
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-01-28 11:45:46 -0500

    NEW COMMIT

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import SideBar from "./_components/SideBar"
 
 function DashboardLayout({children}) {
   return (
     <>
-    <div>{children}</div>
+    <div>
+        <div className='w-64 h-screen fixed'>
+            <SideBar/>
+        </div>
+        <div className='ml-64'>
+            {children}
+        </div>
+    </div>
     </>
   )
 }
