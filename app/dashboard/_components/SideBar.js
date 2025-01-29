5ac4cb04fd88e15397cad7f682d97abf5ded1ab3
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-01-29 11:41:17 -0500

    NEW COMMIT

--- app/dashboard/_components/SideBar.js
+++ app/dashboard/_components/SideBar.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
 import { Button } from '../../../components/ui/button'
+import { Layout } from 'lucide-react'
 
 function SideBar() {
   return (
@@ -9,6 +10,10 @@ function SideBar() {
         <Image src={"/logo2.png"} alt='pdfanywhere' width={200} height={200} className='p-1 mt-3 m-auto'/>
         <div className='mt-10'>
             <Button className="hover:bg-zinc-800 p-5 w-full">+ Upload PDF</Button>
+            <div className='flex gap-2 items-center p-3 mt-5'>
+              <Layout/>
+              <h2>Workspace</h2>
+            </div>
         </div>
     </div>
     </>
