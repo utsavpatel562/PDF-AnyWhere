415a27d96a5f1910fa7f0f7b7d598d0c995713e3
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-02-07 11:46:18 -0500

    NEW COMMIT

--- app/dashboard/_components/UploadPdfDialog.js
+++ app/dashboard/_components/UploadPdfDialog.js
@@ -9,6 +9,7 @@ import {
   } from "../../../components/ui/dialog"
 
   import { Input } from "../../../components/ui/input"
+import { Button } from '../../../components/ui/button'
 
 function UploadPdfDialog({children}) {
   return (
@@ -22,13 +23,16 @@ function UploadPdfDialog({children}) {
       <DialogTitle>Upload PDF File</DialogTitle>
       <DialogDescription asChild>
       <div className=''>
-        <div className='flex mt-5 gap-2 p-3 rounded-md border'>
-          <h2>Select a file to upload</h2>
+      <h2 className='mt-5'>Select a file to upload</h2>
+        <div className='gap-2 p-3 rounded-md border'>
           <input type="file"/>
         </div>
         <div className='mt-2'>
           <label>File Name *</label>
           <Input placeholder="your file name" className="mt-1"/>
+        </div>
+        <div>
+          <Button>Upload</Button>
         </div>
       </div>
       </DialogDescription>
