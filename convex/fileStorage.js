c38d805f1b3f17b1033bff1b05030f03d32d37dd
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-02-12 07:42:35 -0500

    NEW COMMIT

--- convex/fileStorage.js
+++ convex/fileStorage.js
@@ -4,3 +4,12 @@ import { mutation } from "./_generated/server";
 export const generateUploadUrl = mutation(async (ctx) => {
   return await ctx.storage.generateUploadUrl();
 });
+
+export const AddFileEntryToDB = mutation({
+  args: {
+    fileId: v.string(),
+    storageId: v.string(),
+    fileName: v.string(),
+    createdBy: v.string(),
+  }
+})
