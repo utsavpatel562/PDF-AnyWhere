7ad095609ba0b7883ca17977c29fb70dac8d0650
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-02-15 11:40:59 -0500

    NEW COMMIT

--- app/api/pdf-loader/route.js
+++ app/api/pdf-loader/route.js
@@ -1,5 +1,13 @@
 import { NextResponse } from "next/server";
+import { WebPDFLoader } from "@langchain/community/document_loaders/web/pdf";
 
+const pdfUrl="https://adjoining-camel-191.convex.cloud/api/storage/6c5ae850-0a1d-49d3-9ea1-663d891e757a"
 export async function GET(req) {
-    return NextResponse.json({result: "Hello"})
+    // step 1 - load the pdf file
+    const response = await fetch(pdfUrl);
+    const data = await response.blob();
+    const loader = new WebPDFLoader(data);
+    const docs = await loader.load();
+
+    return NextResponse.json({result: docs})
 }
