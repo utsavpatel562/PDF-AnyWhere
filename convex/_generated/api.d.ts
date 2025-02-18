e37ab9899a6b6a7fd24fc7713424095168dabb33
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-02-18 09:32:12 -0500

    NEW COMMIT

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,6 +14,7 @@ import type {
   FunctionReference,
 } from "convex/server";
 import type * as fileStorage from "../fileStorage.js";
+import type * as langchain_db from "../langchain/db.js";
 import type * as user from "../user.js";
 
 /**
@@ -26,6 +27,7 @@ import type * as user from "../user.js";
  */
 declare const fullApi: ApiFromModules<{
   fileStorage: typeof fileStorage;
+  "langchain/db": typeof langchain_db;
   user: typeof user;
 }>;
 export declare const api: FilterApi<
