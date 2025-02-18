7d650692fb85e1122d82fb1241bfb36cdb1ba4b7
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-02-18 09:43:53 -0500

    NEW COMMIT

--- convex/myAction.js
+++ convex/myAction.js
@@ -1,7 +1,6 @@
 "use node";
 
 import { ConvexVectorStore } from "@langchain/community/vectorstores/convex";
-import { OpenAIEmbeddings } from "@langchain/openai";
 import { action } from "./_generated/server.js";
 
 export const ingest = action({
@@ -10,7 +9,6 @@ export const ingest = action({
     await ConvexVectorStore.fromTexts(
       ["Hello world", "Bye bye", "What's this?"],
       [{ prop: 2 }, { prop: 1 }, { prop: 3 }],
-      new OpenAIEmbeddings(),
       { ctx }
     );
   },
