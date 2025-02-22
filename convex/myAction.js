879b2cee260f4da894acc05369abd38d7248e996
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-02-22 11:01:24 -0500

    NEW COMMIT

--- convex/myAction.js
+++ convex/myAction.js
@@ -2,6 +2,8 @@
 
 import { ConvexVectorStore } from "@langchain/community/vectorstores/convex";
 import { action } from "./_generated/server.js";
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import { TaskType } from "@google/generative-ai";
 
 export const ingest = action({
   args: {},
@@ -9,6 +11,11 @@ export const ingest = action({
     await ConvexVectorStore.fromTexts(
       ["Hello world", "Bye bye", "What's this?"],
       [{ prop: 2 }, { prop: 1 }, { prop: 3 }],
+      new GoogleGenerativeAIEmbeddings({
+        model: "text-embedding-004",
+        taskType: TaskType.RETRIEVAL_DOCUMENT,
+        title: "Document title",
+      }),
       { ctx }
     );
   },
