b4eb426215d08d8cb4d28c27ea5ca83171a19866
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-03-02 10:41:20 -0500

    NEW COMMIT

--- app/workspace/[fileId]/_components/TextEditor.js
+++ app/workspace/[fileId]/_components/TextEditor.js
@@ -1,11 +1,19 @@
+import Placeholder from '@tiptap/extension-placeholder'
 import { EditorContent, useEditor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import React from 'react'
 
 function TextEditor() {
     const editor = useEditor({
-        extensions: [StarterKit],
-        content: '<p>Hello World! 🌎️</p>',
+        extensions: [StarterKit, Placeholder.configure({
+            placeholder: "Start Taking Your Notes Here..."
+        })],
+        content: '',
+        editorProps: {
+            attributes: {
+                class: 'focus:outline-none h-screen p-5'
+            }
+        }
       })
   return (
     <div>
