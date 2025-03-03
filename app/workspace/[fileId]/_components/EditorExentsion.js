65fe60d0972fc1b6e93e6bd0c0d01129dbaa8928
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-03-03 10:24:30 -0500

    NEW COMMIT

--- app/workspace/[fileId]/_components/EditorExentsion.js
+++ app/workspace/[fileId]/_components/EditorExentsion.js
@@ -1,4 +1,4 @@
-import { Bold, Italic, Underline } from 'lucide-react';
+import { AlignCenter, AlignJustify, AlignLeft, AlignRight, Bold, Highlighter, Italic, Underline } from 'lucide-react';
 import React from 'react';
 
 function EditorExentsion({ editor }) {
@@ -10,21 +10,51 @@ function EditorExentsion({ editor }) {
         <div className="button-group flex gap-3">
           <button
             onClick={() => editor.chain().focus().toggleBold().run()}
-            className={editor.isActive('bold') ? 'text-blue-700 bg-blue-100 p-2 rounded-sm' : 'p-2'}
+            className={editor.isActive('bold') ? 'text-blue-700 bg-blue-100 p-2 rounded-sm' : 'hover:bg-blue-50 hover:text-blue-700 hover:rounded-sm p-2'}
           >
             <Bold/>
           </button>
           <button
             onClick={() => editor.chain().focus().toggleItalic().run()}
-            className={editor.isActive('italic') ? 'text-blue-700 bg-blue-100 p-2 rounded-sm' : 'p-2'}
+            className={editor.isActive('italic') ? 'text-blue-700 bg-blue-100 p-2 rounded-sm' : 'hover:bg-blue-50 hover:text-blue-700 hover:rounded-sm p-2'}
           >
             <Italic/>
           </button>
           <button
             onClick={() => editor.chain().focus().toggleUnderline().run()}
-            className={editor.isActive('underline') ? 'text-blue-700 bg-blue-100 p-2 rounded-sm' : 'p-2'}
+            className={editor.isActive('underline') ? 'text-blue-700 bg-blue-100 p-2 rounded-sm' : 'hover:bg-blue-50 hover:text-blue-700 hover:rounded-sm p-2'}
           >
             <Underline />
+          </button>
+          <button
+            onClick={() => editor.chain().focus().toggleHighlight().run()}
+            className={editor.isActive('highlight') ? 'text-blue-700 bg-blue-100 p-2 rounded-sm' : 'hover:bg-blue-50 hover:text-blue-700 hover:rounded-sm p-2'}
+          >
+            <Highlighter/>
+          </button>
+          <button
+            onClick={() => editor.chain().focus().setTextAlign('left').run()}
+            className={editor.isActive({ textAlign: 'left' }) ? 'text-blue-700 bg-blue-100 p-2 rounded-sm' : 'hover:bg-blue-50 hover:text-blue-700 hover:rounded-sm p-2'}
+          >
+            <AlignLeft/>
+          </button>
+          <button
+            onClick={() => editor.chain().focus().setTextAlign('center').run()}
+            className={editor.isActive({ textAlign: 'center' }) ? 'text-blue-700 bg-blue-100 p-2 rounded-sm' : 'hover:bg-blue-50 hover:text-blue-700 hover:rounded-sm p-2'}
+          >
+            <AlignCenter/>
+          </button>
+          <button
+            onClick={() => editor.chain().focus().setTextAlign('right').run()}
+            className={editor.isActive({ textAlign: 'right' }) ? 'text-blue-700 bg-blue-100 p-2 rounded-sm' : 'hover:bg-blue-50 hover:text-blue-700 hover:rounded-sm p-2'}
+          >
+            <AlignRight/>
+          </button>
+          <button
+            onClick={() => editor.chain().focus().setTextAlign('justify').run()}
+            className={editor.isActive({ textAlign: 'justify' }) ? 'text-blue-700 bg-blue-100 p-2 rounded-sm' : 'hover:bg-blue-50 hover:text-blue-700 hover:rounded-sm p-2'}
+          >
+            <AlignJustify/>
           </button>
         </div>
       </div>
--- app/workspace/[fileId]/_components/TextEditor.js
+++ app/workspace/[fileId]/_components/TextEditor.js
@@ -4,12 +4,27 @@ import StarterKit from '@tiptap/starter-kit'
 import React from 'react'
 import EditorExentsion from './EditorExentsion'
 import Underline from '@tiptap/extension-underline'
-
+import Highlight from '@tiptap/extension-highlight'
+import Document from '@tiptap/extension-document'
+import Paragraph from '@tiptap/extension-paragraph'
+import Text from '@tiptap/extension-text'
+import TextAlign from '@tiptap/extension-text-align'
+import Heading from '@tiptap/extension-heading'
 function TextEditor() {
     const editor = useEditor({
         extensions: [StarterKit, Placeholder.configure({
             placeholder: "Start Taking Your Notes Here..."
-        }), Underline],
+        }), 
+        Underline, 
+        Highlight, 
+        Document,
+        Paragraph,
+        Text,
+        Heading,
+        TextAlign.configure({
+            types: ['heading', 'paragraph'],
+          }),
+    ],
         content: '',
         editorProps: {
             attributes: {
